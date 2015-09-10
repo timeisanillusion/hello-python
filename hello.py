1c7c805f0458cbc1004f7ae57d8c57ce92abd64b
James <jss42@talk21.com>
2015-09-09 23:06:49 -0400

    Updates on commandline only

--- hello.py
+++ hello.py
@@ -1,19 +1,36 @@
 import os
 import uuid
 from flask import Flask
+import cgi
 
 
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
-BLUE = "#0099FF"
+PURPLE = "#800080"
 GREEN = "#33CC33"
 
-COLOR = GREEN
+COLOR = PURPLE
+
+#Read the file to get initial value
+number = "1"
+
+
+def count():
+    print "Running function"
+    counter = open("counter.txt","r")
+    line = counter.readline()
+    counter.close()
+    number = int(line) + 1
+    counter = open("counter.txt","w")
+    counter.write(str(number))
+    counter.close()
+    print "count now"
+    print str(number)
+    return
 
 @app.route('/')
 def hello():
-
-
+    count()
     return """
     <html>
     <body bgcolor="{}">
@@ -21,12 +38,18 @@ def hello():
     <center><h1><font color="white">Hi, I'm GUID:<br/>
     {}</br>
 
+    <center><h1><font color="white">Welcome <br/>
+    {}</br>
 
     </center>
 
     </body>
     </html>
-    """.format(COLOR,my_uuid,)
+
+
+    """.format(COLOR,my_uuid,number)
+
+
 
 if __name__ == "__main__":
 	app.run(debug=True,host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
